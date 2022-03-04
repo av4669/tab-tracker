14a5afe855e2564dd288418c85bc6ead9201a05f
Aviral Saxena <s057651@bis-school.com>
2022-03-04 19:34:28 +0100

    setting up registration page (UI)

---
+++ client/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input
+      type="email" 
+      name="email"
+      placeholder="email" />
+    <br>
+    <input
+      type="password" 
+      name="password"
+      placeholder="password" />
+    <br>
+    <button>Register</button>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
